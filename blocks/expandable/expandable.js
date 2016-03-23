81f1de97f45c0343660df3bd1f17a2fb681b07c5
Uladzimir Dzmitrachkou <Uladzimir_Dzmitrachkou@epam.com>
2016-03-23 15:54:30 +0300

    updated the expandable block and added the possibility to have different text for heading on mobile and desktop

--- blocks/expandable/expandable.js
+++ blocks/expandable/expandable.js
@@ -3,15 +3,29 @@ $(document).ready(function() {
         event.preventDefault();
         event.stopPropagation();
 
-        if (window.matchMedia('(max-width: 640px)').matches) {
-            $(this).closest('.expandable').toggleClass('expandable_expanded');
+        $(this).closest('.expandable').toggleClass('expandable_expanded');
 
-            if ($(this).hasClass('expandable__expander_toggle-text')) {
-                var currentText = $(this).text();
-                $(this).text(function() {
-                    return currentText === 'Full Details' ? 'Less Details' : 'Full Details';
-                });
-            }
+        if ($(this).hasClass('expandable__expander_toggle-text')) {
+            var currentText = $(this).text();
+            $(this).text(function() {
+                return currentText === 'Full Details' ? 'Less Details' : 'Full Details';
+            });
         }
     });
+});
+
+$(window).on('resize', function() {
+    if (window.matchMedia('(max-width: 640px)').matches) {
+        $('.expandable').each(function() {
+            if ($(this).hasClass('expandable_expanded') && !$(this).hasClass('expandable_mobile')) {
+                $(this).removeClass('expandable_expanded');
+            }
+        })
+    } else if (window.matchMedia('(min-width: 641px)').matches) {
+        $('.expandable_mobile').each(function() {
+           if ($(this).hasClass('expandable_expanded')) {
+               $(this).removeClass('expandable_expanded');
+           }
+        })
+    }
 });
