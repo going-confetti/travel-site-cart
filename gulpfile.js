61ebcb500503d95c2ba8a3938da501e8cb419941
Uladzimir Dzmitrachkou <Uladzimir_Dzmitrachkou@epam.com>
2016-03-29 14:05:53 +0300

    gulp tasks refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,54 @@
 var gulp = require('gulp');
+var runSequence = require('run-sequence');
 var bem = require('gulp-bem');
 var concat = require('gulp-concat');
 var less = require('gulp-less');
 var autoprefixer = require('gulp-autoprefixer');
 var csscomb = require('gulp-csscomb');
 
-var depsResolve = require('deps-resolver');
-var walker = require('bem-walk');
-var techDeps = require('tech-deps.js');
+var levels = ['blocks'];
+var tree = bem(levels);
 
-function levels(levels) {
-    var deps = walker(levels)
-        .pipe(techDeps())
-        .pipe(depsResolve(levels));
-
-    return function (decls) {
-        return deps.resolve(decls);
-    };
-}
-
-
-var deps = levels(['blocks']);
-
-gulp.task('css', function () {
-   return deps([{block: 'page'}])
-       .pipe(bem.src('{block}.less'))
-       .pipe(less())
-       .pipe(concat('index.css'))
-       .pipe(autoprefixer({
-           browsers: ['last 2 versions'],
-           cascade: false
-       }))
-       .pipe(csscomb())
-       .pipe(gulp.dest('dist'));
+gulp.task('less', function() {
+    return tree.deps('blocks/page')
+        .pipe(bem.src('{bem}.less'))
+        .pipe(less())
+        .pipe(concat('index.css'))
+        .pipe(autoprefixer({
+                browsers: ['last 2 versions'],
+                cascade: false
+            }))
+        .pipe(csscomb())
+        .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('html', function () {
-   return deps([{block: 'page'}])
-       .pipe(bem.src('{block}.html'))
+gulp.task('html', function() {
+    return tree.deps('blocks/page')
+       .pipe(bem.src('{bem}.html'))
        .pipe(concat('index.html'))
        .pipe(gulp.dest('dist'));
 });
 
-gulp.task('js', function () {
-   return deps([{block: 'page'}])
-       .pipe(bem.src('{block}.js'))
+gulp.task('js', function() {
+    return tree.deps('blocks/page')
+       .pipe(bem.src('{bem}.js'))
        .pipe(concat('app.js'))
        .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['css', 'html', 'js']);
+gulp.task('build', ['less', 'html', 'js']);
+
+gulp.task('build-dev', function() {
+    runSequence('build', 'watch');
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./blocks/**/*.less', ['less']);
+    gulp.watch('./blocks/**/*.html', ['html']);
+    gulp.watch('./blocks/**/*.js', ['js']);
+});
+
+gulp.task('default', function() {
+    console.log('\nWelcome to gulp');
+    console.log('Use \'gulp build to build the page.\'\n');
+});
